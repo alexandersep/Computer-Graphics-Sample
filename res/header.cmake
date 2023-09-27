a59b4b903fb8602fe94fa900582714ea34b41264
alexander <sepelena@tcd.ie>
2023-09-27 16:28:00 +0100

    fixed for mac users

--- res/header.cmake
+++
@@ -1,9 +0,0 @@
-file(WRITE ${OUTPUT} "")
-string(REPLACE " " ";" RESOURCES ${RESOURCES}) # seperate paths with semi-colons
-file(APPEND ${OUTPUT} "#ifndef __RES_H__\n#define __RES_H__\n") # Create header
-foreach(file ${RESOURCES}) # Loop through files
-    string(REGEX REPLACE "\\/|\\.| |-" "_" name ${file}) # repace /,., ,- with _
-    file(APPEND ${OUTPUT} "extern const char ${name}[];\n") # extern string array
-    file(APPEND ${OUTPUT} "extern const int ${name}_len;\n") # extern size of array
-endforeach()
-file(APPEND ${OUTPUT} "#endif\n") # end header
